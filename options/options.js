4d198d80ca4ceb3260461d0c5532b971067d212c
Crowdbotics <engineering@crowdbotics.com>
2023-07-13 06:27:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-haze-dev-100861",
+  url: "https://flat-haze-dev-100861.botics.co",
+  api: "https://flat-haze-dev-100861.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
